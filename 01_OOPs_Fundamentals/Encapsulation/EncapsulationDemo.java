08d11d920916189759a21c49210f65092452fe11
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2025-05-16 22:34:06 +0530

    .

--- 01_OOPs_Fundamentals/Encapsulation/EncapsulationDemo.java
+++ 01_OOPs_Fundamentals/Encapsulation/EncapsulationDemo.java
@@ -1,5 +1,9 @@
 // EncapsulationDemo.java
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 class Student {
     private String name;
     private int age;
@@ -36,5 +40,59 @@ public class EncapsulationDemo {
 
         System.out.println("Name: " + s.getName());
         System.out.println("Age: " + s.getAge());
+
+        int arr[] = { 2, 2, 2, 2, 2, 3, 3, 4, 5, 5, 6, 6 };
+
+        int br[] = removeDuplicates(arr);
+        System.out.println(Arrays.toString(br));
+
+        String str = "Nitin";
+        System.out.println(checkPallindrome(str));
+
+    }
+
+    // private static boolean checkPallindrome(String str) {
+    //     int index = 0;
+    //     // String str = string.toUpperCase();
+    //     for (int i = str.length() - 1; i >= 0; i--) {
+    //         if (str.charAt(i) != str.charAt(index)) {
+    //             return false;
+    //         }
+    //         index++;
+    //     }
+    //     return true;
+    // }
+
+    private static boolean checkPallindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private static int[] removeDuplicates(int[] arr) {
+        int left = 0;
+        int right = 1;
+        int index = 0;
+        int temp[] = new int[arr.length];
+        while (right < arr.length) {
+            if (arr[left] != arr[right]) {
+                temp[index] = arr[left];
+                left = right;
+                index++;
+            }
+            right++;
+        }
+        // Create a new array with the correct size and copy the elements
+        int result[] = new int[index + 1];
+        System.arraycopy(temp, 0, result, 0, index);
+        result[index] = arr[left]; // Add the last element
+        return result;
     }
 }
