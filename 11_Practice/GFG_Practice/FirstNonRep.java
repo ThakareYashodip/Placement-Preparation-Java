da9a4295290315e80ec47e767024bb1bff7879e5
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2025-05-29 23:06:01 +0530

    ....

--- 11_Practice/GFG_Practice/FirstNonRep.java
+++ 11_Practice/GFG_Practice/FirstNonRep.java
@@ -1,7 +1,8 @@
 public class FirstNonRep {
     public static void main(String[] args) {
         String s1 = "Geeksfrogeeks";
-        System.out.println(nonRepeatingChar(s1));
+        System.out.println(nonRepeatingChar(s1.toLowerCase()));
+        System.out.println(nonRepeatingChar2(s1.toLowerCase()));
     }
 
     public static char nonRepeatingChar(String str) {
@@ -19,5 +20,20 @@ public class FirstNonRep {
             }
         }
         return 0;
+    }
+
+    public static char nonRepeatingChar2(String str) {
+        int[] freq = new int[26];
+
+        for (char ch : str.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (char ch : str.toCharArray()) {
+            if (freq[ch - 'a'] == 1) {
+                return ch;
+            }
+        }
+        return '0';
     }
 }
