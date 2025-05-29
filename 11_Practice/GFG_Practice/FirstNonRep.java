a31f57cf2b6ca10c72ffbc81161c54e0863aa00d
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2025-05-29 23:02:48 +0530

    ...

--- 11_Practice/GFG_Practice/FirstNonRep.java
+++ 11_Practice/GFG_Practice/FirstNonRep.java
@@ -1,5 +1,23 @@
 public class FirstNonRep {
     public static void main(String[] args) {
-        
+        String s1 = "Geeksfrogeeks";
+        System.out.println(nonRepeatingChar(s1));
+    }
+
+    public static char nonRepeatingChar(String str) {
+        int n = str.length();
+        for (int i = 0; i < n; i++) {
+            boolean found = false;
+            for (int j = 0; j < n; j++) {
+                if (i != j && str.charAt(i) == str.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                return str.charAt(i);
+            }
+        }
+        return 0;
     }
 }
