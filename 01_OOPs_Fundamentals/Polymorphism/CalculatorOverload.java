5d2e8e40d080ba93a6061a5a18dab5e15265ae3f
yashodipthakare <ganuthakare99@gmail.com>
2025-06-09 03:13:09 +0530

    .

--- 01_OOPs_Fundamentals/Polymorphism/CalculatorOverload.java
+++ 01_OOPs_Fundamentals/Polymorphism/CalculatorOverload.java
@@ -12,6 +12,9 @@ class Calculator {
     int add(int a, int b, int c) {
         return a + b + c;
     }
+    int add(int a, int b, int d,int m) {
+        return a + b + c + m;
+    }
 }
 
 public class CalculatorOverload {
@@ -20,5 +23,6 @@ public class CalculatorOverload {
         System.out.println(calc.add(5, 3));            // int
         System.out.println(calc.add(2.5, 4.3));        // double
         System.out.println(calc.add(1, 2, 3));         // 3 arguments
+        System.out.println(calc.add(1, 2, 3,4));         // 3 arguments
     }
 }
