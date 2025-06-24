112ffef776e6d6397a41f3f1bbc9e491cbace2b2
yashodipthakare <ganuthakare99@gmail.com>
2025-06-24 23:58:44 +0530

    ..

--- G11_Practice/GFG_Practice/NxtPermutation.java
+++ G11_Practice/GFG_Practice/NxtPermutation.java
@@ -1,7 +1,14 @@
 public class NxtPermutation {
 
     public static void nextPermutation(int [] nums){
-        
+         boolean[] a = new boolean[nums.length];
+        for (int num : nums) {
+            if(a[num]) {
+                return num;
+            }
+            a[num] = true;
+        }
+        return -1;
     }
     public static void main(String[] args) {
         int[] nums = {1,2,3};
